8001627dcbbac72e6ce6a207bfd1a686524254cb
romiocadiz <rocadizc@ucsc.edu>
2023-05-01 10:49:47 -0700

    lab 6
    results of lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,9 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6- Arrays and Objects
+// Author: Romi Ocadiz-Cruz
+// Date: May 1st, 2023
 
 // Constants
+// Define Variables
+var myTransport = [" Mazda v3", " bike", " bus", " Uber", " and rides from friends"];
 
+// Create an object for my main ride
+var myMainRide = {
+  make: "Mazda",
+  model: "v3",
+  color: "Dark Blue",
+  year: 2008,
+  age: function() {
+      return 2023 - this.year;
+  }
+}
+
+// output
+document.writeln("Kinds of transportation I use: ", myTransport,"</br>");
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
