c1190a243a68f7dd7f97158e170bca8d4b4e0ed5
romiocadiz <rocadizc@ucsc.edu>
2023-05-08 11:24:51 -0700

    lab 8
    anon functions

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 8 Anon Functions and Callbacks
+// Author: Romi Ocadiz
+// Date: May 8th, 2023
 
 // Constants
 
+// Variables
+var outputEl = document.getElementById("output"); 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function square(x){
+   return x * x;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//test function
+console.log("The square of 4 is" + square(4));
+console.log("The square of 10 is" + square(10));
 
-// let's get this party started
-main();
+var numbers = [1, 2, 3, 4, 5];
+console.log("My array is" + numbers);
+outputEl.innerHTML = "Original array: [" + numbers + "]<br>";
+
+var result = numbers.map(square);
+// should return [1,4,9,14,25]
+console.log("The squares of my array are" + result);
+outputEl.innerHTML += "The squares of my array are: [" + result + "]<br>";
+
+var result = numbers.map(function(x){
+    return x + x;
+});
+// should return [2,4,6,8,10]
+console.log("The doubles of my array are" + result);
+outputEl.innerHTML += "The doubles of my array are: [" + result + "]<br>";
