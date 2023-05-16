86a4ae966a0c1fc3f611f71bc9554c9b2d06d9c2
romiocadiz <rocadizc@ucsc.edu>
2023-05-15 20:35:03 -0700

    Lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,21 +1,37 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Create a function that takes user input and sorts it.
+// Author: Romi Ocadiz-Cruz
+// Date: May 15th, 2023
 
 // Constants
+// Use the HTML form to create a form that takes user input and sorts it.
+var userName = document.getElementById("user-name").value;
+// Only run the code if the user has entered something
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function sortUserName(userName) {
+  // split string to array
+  var nameArray = userName.split('');
+  // sort the array using localeCompare:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  var nameArraySort = nameArray.sort((a, b) => a.localeCompare(b));
+  // join array back to a string
+  var nameSorted = nameArraySort.join('')
+  return nameSorted;
+ }
+ 
+ // addEventListener - a function that calls sortUserName and writes the
+ // sorted name to the page when the button is clicked
+ document.getElementById("my-button").addEventListener("click", function(event){
+ 
+  // Prevent default behavior (refreshing when submitting form)
+  event.preventDefault();
+   // Get the value of the input field
+  var userName = document.getElementById("user-name").value;
+ 
+  // Only run the code if the user has entered something
+  if(userName){
+    // Output to "output" div element
+    document.getElementById("output").innerHTML = "Oh hey, I've fixed your name: " + sortUserName(userName);
+  }
+ });
