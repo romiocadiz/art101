29671783220daf94b45e516138d6ec27ab6bd914
romiocadiz <rocadizc@ucsc.edu>
2023-05-31 22:35:47 -0700

    lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of this lab is to practice using AJAX.
+// Author: Romi Ocadiz
+// Date: May 31st, 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function fetchDogImage() {
+  // Make a GET request to the Dog API endpoint
+  fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+          // Display the fetched image on the HTML page
+          const resultElement = document.getElementById('result');
+          resultElement.innerHTML = `<img src="${data.message}" alt="Random Dog Image">`;
+      })
+      .catch(error => {
+          console.log('Error:', error);
+      });
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+const dogButton = document.getElementById('dogButton');
 
-// let's get this party started
-main();
+
