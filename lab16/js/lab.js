b8116252bd5e46651345568ce0582bf988fe453d
romiocadiz <rocadizc@ucsc.edu>
2023-06-06 17:35:01 -0700

    lab 16 updates

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,33 +2,27 @@
 // Author: Romi Ocadiz-Cruz
 // Date: June 6th, 2023
 
-var comicObj;
 //using ajax to get the data from the API
 $.ajax({
   //url to the API
     url: "https://xkcd.com/info.0.json",
-    //data to be sent to the server
-    data: {
-      //the data required to get the comic
-      id: 123,
-      api_key: "blahblahblah",
-    },
     //get request
     type: "GET",
     //the type of data expected from the server
     dataType: "json",
     //if the request is successful
     success: function (data) {
-      // success code
-      comicObj(data);
-      // image tag and section title
-      var sectionTitle = $('<h2>').text(comicObj.title); // Create a <h2> element with the title
-      $('body').append(sectionTitle); // Add the section title to the webpage
+      console.log(data);
+    //   // success code
+    //   comicObj(data);
+    //   // image tag and section title
+    //   var sectionTitle = $('<h2>').text(comicObj.title); // Create a <h2> element with the title
+    //   $('body').append(sectionTitle); // Add the section title to the webpage
 
-      var imageTag = $('<img>').attr('src', comicObj.img); // Create an <img> element with the image URL
-      imageTag.attr('alt', comicObj.alt); // Set the alt attribute using the alt key from the object
-      imageTag.attr('title', comicObj.alt); // Set the title attribute using the alt key from the object
-      $('body').append(imageTag); // Add th
+    //   var imageTag = $('<img>').attr('src', comicObj.img); // Create an <img> element with the image URL
+    //   //imageTag.attr('alt', comicObj.alt); // Set the alt attribute using the alt key from the object
+    //  // imageTag.attr('title', comicObj.alt); // Set the title attribute using the alt key from the object
+    //  // $('body').append(imageTag); // Add th
     },
     //if the request fails
     error: function (jqXHR, textStatus, errorThrown) {
@@ -36,6 +30,4 @@ $.ajax({
       console.log("Error:" , textStatus, errorThrown);
     },
   });
-
-  const comicButton = document.getElementById('comicButton');
 
