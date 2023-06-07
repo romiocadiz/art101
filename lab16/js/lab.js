821b02ca4c31fb14d3f594448d38bc76098ad513
romiocadiz <rocadizc@ucsc.edu>
2023-06-06 17:45:20 -0700

    updated lab.js for lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -13,16 +13,15 @@ $.ajax({
     //if the request is successful
     success: function (data) {
       console.log(data);
-    //   // success code
-    //   comicObj(data);
-    //   // image tag and section title
-    //   var sectionTitle = $('<h2>').text(comicObj.title); // Create a <h2> element with the title
-    //   $('body').append(sectionTitle); // Add the section title to the webpage
 
-    //   var imageTag = $('<img>').attr('src', comicObj.img); // Create an <img> element with the image URL
-    //   //imageTag.attr('alt', comicObj.alt); // Set the alt attribute using the alt key from the object
-    //  // imageTag.attr('title', comicObj.alt); // Set the title attribute using the alt key from the object
-    //  // $('body').append(imageTag); // Add th
+      // image tag and section title
+      var sectionTitle = $('<h2>').text(data.title); // Create a <h2> element with the title
+      $('body').append(sectionTitle); // Add the section title to the webpage
+
+      var imageTag = $('<img>').attr('src', data.img); // Create an <img> element with the image URL
+      imageTag.attr('alt', data.alt); // Set the alt attribute using the alt key from the object
+     imageTag.attr('title', data.alt); // Set the title attribute using the alt key from the object
+     $('body').append(imageTag); // Add the image to the webpage
     },
     //if the request fails
     error: function (jqXHR, textStatus, errorThrown) {
